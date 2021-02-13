f97c2b79a60e060ae927d56a45aafa931d21237a
Nikita Ermilov <ermilov1999@gmail.com>
2021-02-13 13:50:07 +0300

    Added JSDoc and arrow-function

--- source/get.js
+++ source/get.js
@@ -1,6 +1,13 @@
 'use strict';
 
-const get = function (object, path_string) {
+/**
+ * Extract property from an object by given property name.
+ * @param {object} object - any object to operate with
+ * @param {string} path_string - full path to the needed property
+ * @return {object} requested property
+ */
+
+const get = (object, path_string) => {
     const path = path_string.split(".").slice(1);
     for (let i = 0; i < path.length && object !== undefined; i++)
         if (path[i])
